8ea6c4637b4318f7b7ec6818a6972b0b179e4160
vineethboini <vineethmudiraj123@icloud.com>
2023-01-21 12:54:18 +0530

    second

--- home/app.js
+++ home/app.js
@@ -1,1 +1,11 @@
-app.js
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text> hello world </Text>
+      </View>
+    )
+  }}
